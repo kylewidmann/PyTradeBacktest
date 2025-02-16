e0074e20d04e83baf33c57336d4b25ac22fb71da
Kyle Widmann <kyle.widmann@gmail.com>
2025-02-16 12:42:31 -0500

    Adding kwargs support for strategies

--- tests/unit/test_backtest.py
+++ tests/unit/test_backtest.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import ANY, MagicMock, patch
 
 import pytest
 
@@ -14,3 +14,16 @@ async def test_backtest_trade_count(mock_order, test_stock_universe):
     await test.run()
 
     assert mock_order.call_count == 66
+
+
+@patch("tests.unit.resources.indicators.SmaCross")
+@pytest.mark.asyncio
+async def test_strategy_kwargs(mock_strategy, test_stock_universe):
+
+    mock_instance = MagicMock()
+    mock_strategy.return_value = mock_instance
+
+    test = Backtest(test_stock_universe, mock_strategy, 10000)
+    await test.run(arg1="test1", arg2="test2")
+
+    mock_strategy.assert_called_with(ANY, ANY, arg1="test1", arg2="test2")
